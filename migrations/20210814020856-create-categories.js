5af75954a008e79b875811a7ee1bb35c161b5821
clean_water <shimizyuta213@gmail.com>
2021-08-20 16:26:35 +0900

    categories_modelの他のモデルとの関係性変更

--- migrations/20210814020856-create-categories.js
+++ migrations/20210814020856-create-categories.js
@@ -9,7 +9,17 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       post_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'cascade', 
+        onDelete: 'cascade', 
+        references: {
+          model:'posts',key:'id'
+        },
+      },
+      category_num: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
